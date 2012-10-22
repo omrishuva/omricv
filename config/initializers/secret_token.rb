28dab6d17f41249ed2c67427f61a264b13a435b7
omri <omri@omri-System-Product-Name.(none)>
2012-10-22 14:27:15 +0200

    init

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+Cv::Application.config.secret_token = '8921deba4e1bf8b8f464ac7e248f59d0a2436de6fbe52a9bd5529bf258b797cb17f4c65337349592559a628c5e22c0567314e05fa54b6f57e6bd3d4712af8a30'
